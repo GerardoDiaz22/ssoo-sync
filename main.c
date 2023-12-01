ed5fbcfdb013dc4964d19f4ccb4f370a9599ab25
Gerardo Diaz <gerardo.diaz@sofoscorp.com>
2023-12-01 06:05:08 -0400

    feat: add counters and metrics for third case

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
 // pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
 // pthread_cancel
@@ -15,6 +17,9 @@ pthread_mutex_t mutex_admins_count;
 int readers_count = 0;
 int admins_count = 0;
 int is_writer_waiting = 0;
+
+#define DAYS 30
+#define HOURS_IN_A_DAY 24
 
 void *read_operation(void *args)
 {
@@ -49,7 +54,9 @@ void *read_operation(void *args)
 
     // SC
     printf("Leyendo...\n");
-    sleep(1);
+    for (int time = 0; time < 10; time++)
+    {
+    }
 
     printf("R - Finalizando SC...\n");
 
@@ -90,7 +97,9 @@ void *write_operation(void *args)
 
     // SC
     printf("Escribiendo...\n");
-    sleep(3);
+    for (int time = 0; time < 30; time++)
+    {
+    }
 
     printf("W - Finalizando SC...\n");
 
@@ -114,7 +123,9 @@ void *admin_operation(void *args)
 
     // SC
     printf("Administrando...\n");
-    sleep(1);
+    for (int time = 0; time < 10; time++)
+    {
+    }
 
     printf("A - Finalizando SC...\n");
 
@@ -128,7 +139,7 @@ void *admin_operation(void *args)
 
 int main()
 {
-    pthread_t t1_read, t2_read, t3_read, t1_write, t2_write, t1_admin, t2_admin;
+    srand(time(NULL));
 
     // Inicializa el mutex en 1
     pthread_mutex_init(&mutex_read, NULL);
@@ -137,30 +148,170 @@ int main()
     pthread_mutex_init(&mutex_admin, NULL);
     pthread_mutex_init(&mutex_admins_count, NULL);
 
-    pthread_create(&t1_read, NULL, read_operation, NULL);
-    pthread_create(&t2_read, NULL, read_operation, NULL);
-    pthread_create(&t3_read, NULL, read_operation, NULL);
-    pthread_create(&t1_write, NULL, write_operation, NULL);
-    pthread_create(&t2_write, NULL, write_operation, NULL);
-    pthread_create(&t1_admin, NULL, admin_operation, NULL);
-    pthread_create(&t2_admin, NULL, admin_operation, NULL);
+    // Entrada de usuario para seleccionar caso de uso
+    int choice;
+    printf("Seleccione un caso de uso:\n");
+    printf("1 - Caso 1\n");
+    printf("2 - Caso 2\n");
+    printf("3 - Caso 3\n");
+    printf("Introduzca su elección: ");
+    scanf("%d", &choice);
 
-    // Caso 1
-    /*pthread_join(t1_read, NULL);
-    pthread_join(t1_write, NULL);
-    sleep(1);
-    pthread_join(t2_read, NULL);
-    pthread_join(t3_read, NULL);*/
+    // Ejecuta el caso de uso seleccionado
+    switch (choice)
+    {
+    case 1:
+        pthread_t a_t1_read, a_t2_read, a_t3_read, a_t1_write, a_t2_write, a_t1_admin, a_t2_admin;
 
-    // Caso 2
-    /*
-    pthread_join(t1_read, NULL);
-    pthread_join(t1_write, NULL);
-    pthread_join(t2_write, NULL);
-    pthread_join(t1_admin, NULL);
-    pthread_join(t2_admin, NULL);
-    pthread_join(t2_read, NULL);
-    pthread_join(t3_read, NULL);*/
+        // Caso 1
+        pthread_create(&a_t1_read, NULL, read_operation, NULL);
+        pthread_create(&a_t1_write, NULL, write_operation, NULL);
+        sleep(1);
+        pthread_create(&a_t2_read, NULL, read_operation, NULL);
+        pthread_create(&a_t3_read, NULL, read_operation, NULL);
+
+        pthread_join(a_t1_read, NULL);
+        pthread_join(a_t1_write, NULL);
+        pthread_join(a_t2_read, NULL);
+        pthread_join(a_t3_read, NULL);
+        break;
+
+    case 2:
+        pthread_t b_t1_read, b_t2_read, b_t3_read, b_t1_write, b_t2_write, b_t1_admin, b_t2_admin;
+
+        // Caso 2
+        pthread_create(&b_t1_read, NULL, read_operation, NULL);
+        pthread_create(&b_t1_write, NULL, write_operation, NULL);
+        pthread_create(&b_t2_write, NULL, write_operation, NULL);
+        pthread_create(&b_t1_admin, NULL, admin_operation, NULL);
+        pthread_create(&b_t2_admin, NULL, admin_operation, NULL);
+        pthread_create(&b_t2_read, NULL, read_operation, NULL);
+        pthread_create(&b_t3_read, NULL, read_operation, NULL);
+
+        pthread_join(b_t1_read, NULL);
+        pthread_join(b_t1_write, NULL);
+        pthread_join(b_t2_write, NULL);
+        pthread_join(b_t1_admin, NULL);
+        pthread_join(b_t2_admin, NULL);
+        pthread_join(b_t2_read, NULL);
+        pthread_join(b_t3_read, NULL);
+        break;
+
+    case 3:
+        int read_operations = 0;
+        int write_operations = 0;
+        int admin_operations = 0;
+
+        int read_operations_hour_array[HOURS_IN_A_DAY] = {0};
+        int write_operations_hour_array[HOURS_IN_A_DAY] = {0};
+        int admin_operations_hour_array[HOURS_IN_A_DAY] = {0};
+
+        for (int i = 0; i < DAYS; i++)
+        {
+            // Generate a random number to decide the dominant operation for the day
+            int dominant_op = rand() % 3; // 0 for read, 1 for write, 2 for admin
+
+            int read_operations_per_hour = 0;
+            int write_operations_per_hour = 0;
+            int admin_operations_per_hour = 0;
+
+            for (int j = 0; j < HOURS_IN_A_DAY; j++)
+            {
+                pthread_t op_thread;
+
+                // Dice roll for read operation
+                int read_dice = rand() % 9 + 1;
+                if (dominant_op == 0)
+                    read_dice += 3; // Bonus for dominant operation
+
+                if (read_dice >= 6) // Threshold for operation to occur
+                {
+                    read_operations_per_hour++;
+                    read_operations_hour_array[j] += 1;
+                    pthread_create(&op_thread, NULL, read_operation, NULL);
+                    pthread_join(op_thread, NULL);
+                }
+
+                // Dice roll for write operation
+                int write_dice = rand() % 9 + 1;
+                if (dominant_op == 1)
+                    write_dice += 3; // Bonus for dominant operation
+
+                if (write_dice >= 6) // Threshold for operation to occur
+                {
+                    write_operations_per_hour++;
+                    write_operations_hour_array[j] += 1;
+                    pthread_create(&op_thread, NULL, write_operation, NULL);
+                    pthread_join(op_thread, NULL);
+                }
+
+                // Dice roll for admin operation
+                int admin_dice = rand() % 9 + 1;
+                if (dominant_op == 2)
+                    admin_dice += 3; // Bonus for dominant operation
+
+                if (admin_dice >= 6) // Threshold for operation to occur
+                {
+                    admin_operations_per_hour++;
+                    admin_operations_hour_array[j] += 1;
+                    pthread_create(&op_thread, NULL, admin_operation, NULL);
+                    pthread_join(op_thread, NULL);
+                }
+            }
+
+            read_operations += read_operations_per_hour;
+            write_operations += write_operations_per_hour;
+            admin_operations += admin_operations_per_hour;
+
+            printf("Operación dominante: %d\n", dominant_op);
+        }
+
+        printf("Operaciones de lectura: %d\n", read_operations);
+        printf("Operaciones de escritura: %d\n", write_operations);
+        printf("Operaciones de administración: %d\n", admin_operations);
+
+        printf("Operaciones de lectura por hora: %f\n", (float)read_operations / (DAYS * HOURS_IN_A_DAY));
+        printf("Operaciones de escritura por hora: %f\n", (float)write_operations / (DAYS * HOURS_IN_A_DAY));
+        printf("Operaciones de administración por hora: %f\n", (float)admin_operations / (DAYS * HOURS_IN_A_DAY));
+
+        // Encontrar hora con más operaciones
+        int max_read_operations_hour = 0;
+        int max_write_operations_hour = 0;
+        int max_admin_operations_hour = 0;
+
+        int max_read_operations_hour_index = 0;
+        int max_write_operations_hour_index = 0;
+        int max_admin_operations_hour_index = 0;
+
+        for (int i = 0; i < HOURS_IN_A_DAY; i++)
+        {
+            if (read_operations_hour_array[i] > max_read_operations_hour)
+            {
+                max_read_operations_hour = read_operations_hour_array[i];
+                max_read_operations_hour_index = i;
+            }
+
+            if (write_operations_hour_array[i] > max_write_operations_hour)
+            {
+                max_write_operations_hour = write_operations_hour_array[i];
+                max_write_operations_hour_index = i;
+            }
+
+            if (admin_operations_hour_array[i] > max_admin_operations_hour)
+            {
+                max_admin_operations_hour = admin_operations_hour_array[i];
+                max_admin_operations_hour_index = i;
+            }
+        }
+
+        printf("Hora con más operaciones de lectura: %d\n", max_read_operations_hour_index);
+        printf("Hora con más operaciones de escritura: %d\n", max_write_operations_hour_index);
+        printf("Hora con más operaciones de administración: %d\n", max_admin_operations_hour_index);
+
+        break;
+    default:
+        printf("Opción no válida.\n");
+    }
 
     pthread_mutex_destroy(&mutex_read);
     pthread_mutex_destroy(&mutex_readers_count);
